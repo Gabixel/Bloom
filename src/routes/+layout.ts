e96098bd119d09cb6c2b299a7cfc0039fba83d9f
Gabriel <43073074+Gabixel@users.noreply.github.com>
2026-07-11 21:12:46 +0000

    Allow navigating the app window history

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,12 @@
+import { App as CapacitorApp } from "@capacitor/app";
 import { cconsole } from "../lib/logger.svelte";
 
 cconsole.log("Hello from layout.ts");
+
+CapacitorApp.addListener("backButton", ({ canGoBack }) => {
+	if (!canGoBack) {
+		CapacitorApp.exitApp();
+	} else {
+		window.history.back();
+	}
+});
