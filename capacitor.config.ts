375864814394dff2cea5c3c9649b109655e97ff3
Gabriel <43073074+Gabixel@users.noreply.github.com>
2026-07-11 21:05:01 +0000

    Add more Capacitor settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,32 @@ const config: CapacitorConfig = {
 			enabled: true,
 		},
 	},
+	ios: {
+		limitsNavigationsToAppBoundDomains: true,
+	},
+	android: {
+		allowMixedContent: true,
+		buildOptions: {
+			releaseType: "APK",
+		},
+		// zoomEnabled: false,
+		// appendUserAgent: "Bloom/1.0",
+		// backgroundColor: "#2e2e2e",
+		initialFocus: true,
+		resolveServiceWorkerRequests: false,
+	},
+	appendUserAgent: "Bloom/1.0",
+	loggingBehavior: "none",
+	backgroundColor: "#2e2e2e",
+	zoomEnabled: false,
+	server: {
+		cleartext: true,
+		androidScheme: "https",
+		// androidScheme: "http",
+		// allowNavigation: [
+		//   "localhost"
+		// ]
+	},
 };
 
 export default config;
