16adccad95be0b2c974bceb2016f0d60455aedef
Gabriel <43073074+Gabixel@users.noreply.github.com>
2026-08-05 10:23:35 +0000

    Another try preventing soft keyboards from resizing viewport

--- android/app/src/main/java/com/gabigroup/bloom/MainActivity.java
+++ android/app/src/main/java/com/gabigroup/bloom/MainActivity.java
@@ -2,6 +2,7 @@ package com.gabigroup.bloom;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebSettings;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -11,8 +12,20 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-		// thanks https://forum.ionicframework.com/t/android-disable-scroll-bounce-effect/231528
         WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+
+		// https://forum.ionicframework.com/t/android-disable-scroll-bounce-effect/231528
         webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+
+        // https://stackoverflow.com/a/47148988/16804863
+        // "viewport" seems to get ignored by default:
+        // https://developer.chrome.com/blog/viewport-resize-behavior#:~:text=These%20changes%20do%20not%20affect%20WebView
+        settings.setUseWideViewPort(true); // https://developer.android.com/reference/android/webkit/WebSettings#getUseWideViewPort()
+        settings.setLoadWithOverviewMode(true);
+
+
+        settings.setLoadsImagesAutomatically(false);
+        settings.setMediaPlaybackRequiresUserGesture(false);
     }
 }
