2f3929e5b4540aa352aa994fe6037205c2052d03
Gabriel Del Nero <gabriel.gabixel@outlook.com>
2026-08-31 15:40:55 +0200

    Improve loading icon animation on mobile (and remove asset)

--- src/lib/layouts/ui/LoadingIcon.svelte
+++ src/lib/layouts/ui/LoadingIcon.svelte
@@ -1,11 +1,241 @@
 <script lang="ts">
-	import icon from "$lib/assets/loading-note.svg?raw";
-
 	let { style }: { style?: string } = $props();
 </script>
 
 <div {style}>
-	{@html icon}
+	<svg
+		width="320"
+		height="320"
+		viewBox="0 0 320 320"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<style>
+			@keyframes kf_rect2987_transform_0 {
+				0% {
+					animation-timing-function: cubic-bezier(
+						0.3,
+						-0.05,
+						0.7,
+						-0.5
+					);
+					transform: translateX(267px) translateY(111.037px)
+						translateX(-133px);
+				}
+				44.44% {
+					transform: translateX(267px) translateY(111.037px)
+						translateX(-267px);
+				}
+				61.11% {
+					animation-timing-function: linear;
+					transform: translateX(267px) translateY(111.037px)
+						translateX(-133px);
+				}
+				100% {
+					transform: translateX(267px) translateY(111.037px)
+						translateX(-133px);
+				}
+			}
+			@keyframes kf_rect2987_opacity_0 {
+				0% {
+					animation-timing-function: linear;
+					opacity: 1;
+				}
+				27.78% {
+					animation-timing-function: ease-out;
+					opacity: 1;
+				}
+				44.44% {
+					animation-timing-function: linear;
+					opacity: 0;
+				}
+				61.11% {
+					opacity: 0;
+				}
+				61.12% {
+					opacity: 1;
+				}
+				100% {
+					opacity: 1;
+				}
+			}
+			#rect2987 {
+				transform-origin: 0 0;
+				animation:
+					kf_rect2987_transform_0 0.9s linear infinite,
+					kf_rect2987_opacity_0 0.9s linear infinite;
+			}
+			@keyframes kf_rect2987_2_transform_0 {
+				0% {
+					transform: translateX(267px) translateY(111.037px)
+						translateX(0px);
+				}
+				27.78% {
+					animation-timing-function: cubic-bezier(0.45, 1.45, 0.8, 1);
+					transform: translateX(267px) translateY(111.037px)
+						translateX(0px);
+				}
+				61.11% {
+					transform: translateX(267px) translateY(111.037px)
+						translateX(-133px);
+				}
+				61.12% {
+					transform: translateX(267px) translateY(111.037px)
+						translateX(-133px);
+				}
+				100% {
+					transform: translateX(267px) translateY(111.037px)
+						translateX(0px);
+				}
+			}
+			@keyframes kf_rect2987_2_opacity_0 {
+				0% {
+					animation-timing-function: linear;
+					opacity: 0;
+				}
+				27.78% {
+					animation-timing-function: ease-out;
+					opacity: 0;
+				}
+				61.11% {
+					animation-timing-function: linear;
+					opacity: 1;
+				}
+				61.12%{
+					opacity: 0;
+				}
+				100% {
+					opacity: 0;
+				}
+			}
+			#rect2987_2 {
+				opacity: 0;
+				transform-origin: 0 0;
+				animation:
+					kf_rect2987_2_transform_0 0.9s linear infinite,
+					kf_rect2987_2_opacity_0 0.9s linear infinite;
+			}
+		</style>
+		<g id="Music-eighthnote_4" clip-path="url(#clip0_92_66)">
+			<path
+				id="rect3768"
+				opacity="0.7"
+				transform="translate(0 109.547)"
+				d="M319.972 0H0V3.70563H319.972V0Z"
+				fill="url(#paint0_linear_92_66)"
+			/>
+			<path
+				id="rect3774"
+				opacity="0.7"
+				transform="translate(0 133.43)"
+				d="M319.972 0H0V3.70563H319.972V0Z"
+				fill="url(#paint1_linear_92_66)"
+			/>
+			<path
+				id="rect3776"
+				opacity="0.7"
+				transform="translate(0.136719 157.485)"
+				d="M319.972 0H0V3.70563H319.972V0Z"
+				fill="url(#paint2_linear_92_66)"
+			/>
+			<path
+				id="rect3778"
+				opacity="0.7"
+				transform="translate(0 181.54)"
+				d="M319.972 0H0V3.70563H319.972V0Z"
+				fill="url(#paint3_linear_92_66)"
+			/>
+			<path
+				id="rect3780"
+				opacity="0.7"
+				transform="translate(0 205.509)"
+				d="M319.972 0H0V3.70563H319.972V0Z"
+				fill="url(#paint4_linear_92_66)"
+			/>
+			<path
+				id="rect2987"
+				transform="translate(267 111.037)"
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M26.1644 0V74.5312C23.9701 71.3984 15.0462 70.9265 8.78942 74.375C1.37399 78.4621 -2.00814 86.1261 1.22692 91.5C4.46199 96.8739 13.0927 97.9309 20.5082 93.8438C24.9597 91.3902 27.9269 87.639 28.9144 83.875C29.3669 81.8062 29.103 80.7329 29.0707 79.2812V26.625C61.6047 34.8665 46.5899 63.4361 43.6633 73.5278C70.3911 37.022 29.4352 22.1318 29.0707 0H26.1644Z"
+				fill="#FFE06A"
+			/>
+			<path
+				id="rect2987_2"
+				opacity="0"
+				transform="translate(267 111.037)"
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M26.1644 0V74.5312C23.9701 71.3984 15.0462 70.9265 8.78942 74.375C1.37399 78.4621 -2.00814 86.1261 1.22692 91.5C4.46199 96.8739 13.0927 97.9309 20.5082 93.8438C24.9597 91.3902 27.9269 87.639 28.9144 83.875C29.3669 81.8062 29.103 80.7329 29.0707 79.2812V26.625C61.6047 34.8665 46.5899 63.4361 43.6633 73.5278C70.3911 37.022 29.4352 22.1318 29.0707 0H26.1644Z"
+				fill="#FFE06A"
+			/>
+		</g>
+		<defs>
+			<linearGradient
+				id="paint0_linear_92_66"
+				x1="0"
+				y1="1.85281"
+				x2="319.972"
+				y2="1.85281"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop offset="0.1" stop-color="#FFE06A" stop-opacity="0" />
+				<stop offset="0.5" stop-color="#D1A700" />
+				<stop offset="0.9" stop-color="#FFE06A" stop-opacity="0" />
+			</linearGradient>
+			<linearGradient
+				id="paint1_linear_92_66"
+				x1="0"
+				y1="1.85281"
+				x2="319.972"
+				y2="1.85281"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop offset="0.1" stop-color="#FFE06A" stop-opacity="0" />
+				<stop offset="0.5" stop-color="#D1A700" />
+				<stop offset="0.9" stop-color="#FFE06A" stop-opacity="0" />
+			</linearGradient>
+			<linearGradient
+				id="paint2_linear_92_66"
+				x1="0"
+				y1="1.85281"
+				x2="319.972"
+				y2="1.85281"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop offset="0.1" stop-color="#FFE06A" stop-opacity="0" />
+				<stop offset="0.5" stop-color="#D1A700" />
+				<stop offset="0.9" stop-color="#FFE06A" stop-opacity="0" />
+			</linearGradient>
+			<linearGradient
+				id="paint3_linear_92_66"
+				x1="0"
+				y1="1.85281"
+				x2="319.972"
+				y2="1.85281"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop offset="0.1" stop-color="#FFE06A" stop-opacity="0" />
+				<stop offset="0.5" stop-color="#D1A700" />
+				<stop offset="0.9" stop-color="#FFE06A" stop-opacity="0" />
+			</linearGradient>
+			<linearGradient
+				id="paint4_linear_92_66"
+				x1="0"
+				y1="1.85281"
+				x2="319.972"
+				y2="1.85281"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop offset="0.1" stop-color="#FFE06A" stop-opacity="0" />
+				<stop offset="0.5" stop-color="#D1A700" />
+				<stop offset="0.9" stop-color="#FFE06A" stop-opacity="0" />
+			</linearGradient>
+			<clipPath id="clip0_92_66">
+				<rect width="320" height="320" fill="white" />
+			</clipPath>
+		</defs>
+	</svg>
 </div>
 
 <style>
@@ -16,9 +246,10 @@
 		margin-inline: auto;
 		--wait: 0.75s;
 		animation: wait-for-fade 0.45s ease var(--wait) normal forwards;
+		/*TODO: second animation to slow down the other one(?) */
 	}
 
-	div :global(svg) {
+	div svg {
 		width: 100%;
 		height: 100%;
 	}
